db2d1cfbd6976cc1b422b9c9341adacfd6db0cd0
Schlitzohr101 <wemx836@gmail.com>
2019-11-13 15:07:05 -0800

    part 2 is questionably complete

--- AliceReader.java
+++ AliceReader.java
@@ -2,54 +2,80 @@ import java.io.*;
 import java.util.*;
 import java.time.Duration;
 import java.time.Instant;
-        /*Instant start = Instant.now();
-         Instant end = Instant.now();
-         Duration timeElapsed = Duration.between(start, end);
-         System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
-        */
+
 public class AliceReader {
-    public static void main(String[] args) throws FileNotFoundException {
-                AliceHash();
-                // AliceTree();
+    public static void main(String[] args) {
+        if (args.length != 0) {
+            Set<String> set = new HashSet<String>();
+            switch (Character.toUpperCase(args[0].charAt(0))) {
+            case 'H':
+                System.out.println("Working with the Hash Set");
+                set = new HashSet<String>();
+                break;
+            case 'T':
+                System.out.println("Working with the Tree Set");
+                set = new TreeSet<String>();
+                break;
+            default:
+                System.out.println("Inproper argument specified, please use \"Hash\" or \"Tree\" as an argument\nUsing Hash as default");
+                break;
             }
 
-            public static void AliceHash() throws FileNotFoundException {
-                int count = 0;
-                //try:
+            try {
                 Scanner alicein = new Scanner(new File("Alice.txt"));
-                Set<String> hash = new HashSet<>();
                 Instant start = Instant.now();
                 while (alicein.hasNext()) {
                     String word = alicein.next();
-                    hash.add(word);
+                    set.add(word);
                 }
-                System.out.println(count);
                 Instant end = Instant.now();
                 Duration timeElapsed = Duration.between(start, end);
-                System.out.println("Time taken to insert to hashset: " + timeElapsed.toMillis() + " milliseconds");
-                //catch throws FNF
-                //Sys.out~~~~~ "Error File doesnt exist"
-
-                //Hash word Searcher
-
-                //Scanner in = new Scanner(System.in);
-                //System.out.println("Enter a word to be hash searched");
-                Iterator<String> iter = hash.iterator();
-                String input = "alice";
-                Instant start2 = Instant.now();
-                while(iter.hasNext()) {
-                    String word = iter.next();
-                    while (count < 100) {
-                        if (word.equals(input)) {
-                            count++;
-                        }
-                    }
-                    System.out.println(count);
-                    Instant end2 = Instant.now();
-                    Duration timeElapsed2 = Duration.between(start2, end2);
-                    System.out.println("Time taken to search hashset: " + timeElapsed2.toMillis() + " milliseconds");
-                }
-                }
-
+                System.out.println("Time taken to insert to set: " + timeCalculator(timeElapsed.toNanos()));
+                alicein.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("Error finding file\n");
+                e.printStackTrace();
             }
 
+            //System.out.println(set);
+            String input = "Alice";
+            System.out.println("Seaching for 100 instances of: " + input);
+            int count = 0;
+            boolean found = false;
+            Iterator<String> iter = set.iterator();
+            Instant start2 = Instant.now();
+            while(iter.hasNext() && !found) {
+                if(iter.next().contains(input)) {
+                    count++;
+                }
+                if (count > 100) {
+                    found = true;
+                }
+            }
+            Instant end2 = Instant.now();
+            System.out.println("100 instances of: \""+input+"\" were "+(found?"found ":"not found"));
+            Duration timeElapsed2 = Duration.between(start2, end2);
+            System.out.println("Time taken to search set: " + timeCalculator(timeElapsed2.toNanos()));
+        } else {
+            System.out.println("No agruements specified\nPlease use \"Hash\" or \"Tree\"");
+        }
+
+        // AliceTree();
+    }
+
+    public static String timeCalculator(long t) {
+        StringBuilder builder = new StringBuilder();
+        // builder.append("Your time is ");
+        if (t / Math.pow(10.0, 9.0) > 1) {
+            builder.append(t / Math.pow(10.0, 9.0) + " seconds\n");
+        } else if (t / Math.pow(10.0, 6.0) > 1) {
+            builder.append(t / Math.pow(10.0, 6.0) + " milli seconds\n");
+        } else if (t / Math.pow(10.0, 3.0) > 1) {
+            builder.append(t / Math.pow(10.0, 3.0) + " micro seconds!!!\n");
+        } else {
+            builder.append(t + " nano seconds!!!\n");
+        }
+        return builder.toString();
+    }
+
+}
--- Collection.java
+++ Collection.java
@@ -5,94 +5,99 @@ import java.time.Instant;
 
 public class Collection {
     public static void main(String[] args) {
-        Map<String, Integer> wordMap = new TreeMap<String, Integer>();
-        Map<Character, Integer> pointMap = new HashMap<Character,Integer>();
-        if (args[0].equals("Tree")) {
-            System.out.println("Using TreeMap as data structure");
-            wordMap = new TreeMap<String, Integer>();
-            pointMap = new TreeMap<Character,Integer>();
-        } else if (args[0].equals("Hash")) {
-            System.out.println("Using HashMap as data structure");
-            wordMap = new HashMap<String, Integer>();
-            pointMap = new HashMap<Character,Integer>();
-        }
-        Scanner in = new Scanner(System.in);
-        try {
-            in = new Scanner(new File("QNoU.txt"));
-            System.out.println("Creating Map from file");
+        if (args.length != 0) {
+            Map<String, Integer> wordMap = new TreeMap<String, Integer>();
+            Map<Character, Integer> pointMap = new HashMap<Character, Integer>();
+            if (args[0].equals("Tree")) {
+                System.out.println("Using TreeMap as data structure");
+                wordMap = new TreeMap<String, Integer>();
+                pointMap = new TreeMap<Character, Integer>();
+            } else if (args[0].equals("Hash")) {
+                System.out.println("Using HashMap as data structure");
+                wordMap = new HashMap<String, Integer>();
+                pointMap = new HashMap<Character, Integer>();
+            }
+            Scanner in = new Scanner(System.in);
+            try {
+                in = new Scanner(new File("QNoU.txt"));
+                System.out.println("Creating Map from file");
+                Instant start = Instant.now();
+                while (in.hasNext()) {
+                    wordMap.put(in.nextLine(), 0);
+                }
+                Instant end = Instant.now();
+                Duration timeElapsed = Duration.between(start, end);
+                System.out.println("Time taken: " + timeCalculator(timeElapsed.toNanos()));
+                in.close();
+
+            } catch (FileNotFoundException e) {
+                System.out.println("Unable to open file");
+                e.printStackTrace();
+            } catch (InputMismatchException e) {
+                System.out.println("Incorrect in.next has occured, either wrong amount or wrong .nextXXXX");
+                e.printStackTrace();
+            } catch (Exception e) {
+                System.out.println("Unknown error has occured");
+                e.printStackTrace();
+            }
+
+            try {
+                in = new Scanner(new File("Scrabble.txt"));
+                while (in.hasNext()) {
+                    pointMap.put(Character.toUpperCase(in.next().charAt(0)), in.nextInt());
+                }
+
+                in.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("Unable to open Scrabble file");
+                e.printStackTrace();
+            } catch (InputMismatchException e) {
+                System.out.println("Error with accessing data in file with in.next, check syntax");
+                e.printStackTrace();
+            } catch (Exception e) {
+                System.out.println("Unknown error has occured");
+                e.printStackTrace();
+            }
+
+            Iterator<Map.Entry<String, Integer>> wordIt = wordMap.entrySet().iterator();
+            System.out.println("Updating point values");
             Instant start = Instant.now();
-            while (in.hasNext()) {
-                wordMap.put(in.nextLine(), 0);
+            while (wordIt.hasNext()) {
+                Map.Entry<String, Integer> pair = wordIt.next();
+                int pointTotal = 0;
+                for (char c : pair.getKey().toCharArray()) {
+                    c = Character.toUpperCase(c);
+                    pointTotal += pointMap.get(c);
+                }
+                wordMap.replace(pair.getKey(), 0, pointTotal);
             }
             Instant end = Instant.now();
+            Iterator<Map.Entry<String, Integer>> it = wordMap.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry<String, Integer> pair = it.next();
+                System.out.printf("key: %-15s", pair.getKey());
+                System.out.println(" value: " + pair.getValue());
+            }
             Duration timeElapsed = Duration.between(start, end);
             System.out.println("Time taken: " + timeCalculator(timeElapsed.toNanos()));
             in.close();
-
-        } catch (FileNotFoundException e) {
-            System.out.println("Unable to open file");
-            e.printStackTrace();
-        } catch (InputMismatchException e) {
-            System.out.println("Incorrect in.next has occured, either wrong amount or wrong .nextXXXX");
-            e.printStackTrace();
-        } catch (Exception e) {
-            System.out.println("Unknown error has occured");
-            e.printStackTrace();
+        } else {
+            System.out.println("No arguments specified, exiting application.\nPlease run again with either \"Hash\" or \"Tree\" Specified");
         }
-
-        try {
-            in = new Scanner(new File("Scrabble.txt"));
-            while (in.hasNext()) {
-                pointMap.put(Character.toUpperCase(in.next().charAt(0)), in.nextInt()) ;
-            }
-
-            in.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Unable to open Scrabble file");
-            e.printStackTrace();
-        } catch (InputMismatchException e) {
-            System.out.println("Error with accessing data in file with in.next, check syntax");
-            e.printStackTrace();
-        } catch (Exception e) {
-            System.out.println("Unknown error has occured");
-            e.printStackTrace();
-        }
-
-        Iterator<Map.Entry<String, Integer>> wordIt = wordMap.entrySet().iterator();
-        System.out.println("Updating point values");
-        Instant start = Instant.now();
-        while (wordIt.hasNext()) {
-            Map.Entry<String, Integer> pair = wordIt.next();
-            int pointTotal = 0;
-            for (char c : pair.getKey().toCharArray()) {
-                c = Character.toUpperCase(c);
-                pointTotal += pointMap.get(c);
-            }
-            wordMap.replace(pair.getKey(), 0, pointTotal);
-        }
-        Instant end = Instant.now();
-        Iterator<Map.Entry<String, Integer>> it = wordMap.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry<String, Integer> pair = it.next();
-            System.out.printf("key: %-15s", pair.getKey());
-            System.out.println(" value: " + pair.getValue());
-        }
-        Duration timeElapsed = Duration.between(start, end);
-        System.out.println("Time taken: " + timeCalculator(timeElapsed.toNanos()));
-        in.close();
     }
-	public static String timeCalculator(long t) {
-		StringBuilder builder = new StringBuilder();
-		// builder.append("Your time is ");
-		if (t/Math.pow(10.0, 9.0) > 1) {
-			builder.append(t/Math.pow(10.0, 9.0) + " seconds\n");
-		} else if (t/Math.pow(10.0, 6.0) > 1) {
-			builder.append(t/Math.pow(10.0, 6.0) + " milli seconds\n");
-		} else if (t/Math.pow(10.0, 3.0) > 1) {
-			builder.append(t/Math.pow(10.0, 3.0) + " micro seconds!!!\n");
-		} else {
-			builder.append(t + " nano seconds!!!\n");
-		}
-		return builder.toString();
-	}
+
+    public static String timeCalculator(long t) {
+        StringBuilder builder = new StringBuilder();
+        // builder.append("Your time is ");
+        if (t / Math.pow(10.0, 9.0) > 1) {
+            builder.append(t / Math.pow(10.0, 9.0) + " seconds\n");
+        } else if (t / Math.pow(10.0, 6.0) > 1) {
+            builder.append(t / Math.pow(10.0, 6.0) + " milli seconds\n");
+        } else if (t / Math.pow(10.0, 3.0) > 1) {
+            builder.append(t / Math.pow(10.0, 3.0) + " micro seconds!!!\n");
+        } else {
+            builder.append(t + " nano seconds!!!\n");
+        }
+        return builder.toString();
+    }
 }
